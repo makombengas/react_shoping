2e8b70d7b212a0bd652b0a9cfc7c3cce26a30a9d
makombengas <berlioz2001@yahoo.fr>
2023-05-17 18:12:45 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Typescript Mern Dorette
+
+## Works infos
+
+1. Introduction
+2. Installations tools
+3. Creation TypeScript React App by vite
+4. Create Git Repository
---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>TS Amazona</header>
+      <main></main>
+
+      <footer>all rights reserved</footer>
+    </div>
+  );
+}
+
+export default App;
