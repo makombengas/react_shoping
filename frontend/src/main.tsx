58fd54f3c86f8ae20a77963c2f430d984ce45c47
makombengas <berlioz2001@yahoo.fr>
2023-05-18 20:26:21 +0200

    change on gitignore and build some components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import App from './App.tsx';
 import './index.css';
-
+import axios from 'axios';
+axios.defaults.baseURL =
+  process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : '/';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
---
+++ frontend/src/types/ApiError.ts
@@ -0,0 +1,6 @@
+export declare type ApiError = {
+  message: string;
+  response: {
+    data: { message: string };
+  };
+};
